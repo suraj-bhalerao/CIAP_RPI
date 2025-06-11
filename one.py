168bb10efa3139b450cba48e07150452b778b735
surajB2024 <surajbhalerao2024@gmail.com>
2025-06-11 15:33:30 +0530

    one.py - 0 byte file logic removed

--- one.py
+++ one.py
@@ -65,21 +65,21 @@ def upload_to_onedrive(file_path, hostname, today_date):
         print(f"Error during upload: {e}")
 
 #### this functions is used to check zero size and currupted files
-# def is_log_file_valid(file_path):
-#     try:
-#         if os.path.getsize(file_path) == 0:
-#             print(f"Skipping empty file: {file_path}")
-#             return False
-#         with open(file_path, 'r', errors='ignore') as f:
-#             content = f.read(1024)  # Read first 1KB
-#             if not any(c.isprintable() for c in content):
-#                 print(f"Skipping possibly corrupted file (unprintable): {file_path}")
-#                 return False
-#         return True
-#     except Exception as e:
-#         print(f"Error validating file {file_path}: {e}")
-#         return False
-
+ def is_log_file_valid(file_path):
+     try:
+         if os.path.getsize(file_path) == 0:
+             print(f"Skipping empty file: {file_path}")
+             return False
+         with open(file_path, 'r', errors='ignore') as f:
+             content = f.read(1024)  # Read first 1KB
+             if not any(c.isprintable() for c in content):
+                 print(f"Skipping possibly corrupted file (unprintable): {file_path}")
+                 return False
+         return True
+     except Exception as e:
+         print(f"Error validating file {file_path}: {e}")
+         return False
+### this to check the file is empty
 def is_non_empty_file(file_path):
     return os.path.getsize(file_path) > 0
 
@@ -102,9 +102,9 @@ def main():
                 continue
             # if not is_log_file_valid(log_file_path):
             #     continue
-            if not is_non_empty_file(log_file_path):
-                print(f"Skipping empty file: {log_file}")
-                continue
+            #if not is_non_empty_file(log_file_path):
+            #    print(f"Skipping empty file: {log_file}")
+            #    continue
             upload_to_onedrive(log_file_path, hostname, today_date)
     else:
         print("Not connected to Wi-Fi.")
